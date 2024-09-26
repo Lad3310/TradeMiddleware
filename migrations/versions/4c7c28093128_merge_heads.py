d3c199898560025d07415234d6ce0f5e90e6b7e3
JLad3310 <35703915-JLad3310@users.noreply.replit.com>
2024-09-26 17:26:36 +0000

    Agent query: What is the full output of the 'flask db upgrade' command?

---
+++ migrations/versions/4c7c28093128_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 4c7c28093128
+Revises: 
+Create Date: 2024-09-26 17:21:49.387455
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4c7c28093128'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/8c96c87b8fba_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 8c96c87b8fba
+Revises: 
+Create Date: 2024-09-26 17:24:58.394014
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c96c87b8fba'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
